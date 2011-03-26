a11992d5e7ab5eea6fd60693b9690ca42886ecda
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2011-03-26 10:08:29 +0100

    UrlExpander emits "singleUrlExpanded" event

--- test/tests/url_expander.test.js
+++ test/tests/url_expander.test.js
@@ -46,5 +46,45 @@ exports['UrlExpander#expand'] = testCase({
         expander.expand();
 
         test.done();
+    },
+    'emits "singleUrlExpanded" event after a single url has been expanded': function (test) {
+        test.expect(2);
+
+        var originalUrl = 'http://example.com';
+        var expandedUrl = 'http://example.com/expanded';
+
+        var expander = new UrlExpander([originalUrl]);
+        expander.SingleUrlExpander = function () {};
+        expander.SingleUrlExpander.prototype = new SingleUrlExpanderMock([originalUrl], [expandedUrl]);
+
+        expander.on('singleUrlExpanded', function (origUrl, expUrl) {
+            test.equal(originalUrl, origUrl);
+            test.equal(expandedUrl, expUrl);
+        });
+
+        expander.expand();
+
+        test.done();
+    },
+    'emits "singleUrlExpanded" event after each url has been expanded': function (test) {
+        test.expect(4);
+
+        var originalUrls = ['http://example.com', 'http://example.org'];
+        var expandedUrls = ['http://example.com/expanded', 'http://example.org/expanded'];
+        var eventCounter = 0;
+
+        var expander = new UrlExpander(originalUrls);
+        expander.SingleUrlExpander = function () {};
+        expander.SingleUrlExpander.prototype = new SingleUrlExpanderMock(originalUrls, expandedUrls);
+
+        expander.on('singleUrlExpanded', function (originalUrl, expandedUrl) {
+            test.equal(originalUrls[eventCounter], originalUrl);
+            test.equal(expandedUrls[eventCounter], expandedUrl);
+            eventCounter++;
+        });
+
+        expander.expand();
+
+        test.done();
     }
 });
